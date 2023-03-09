30181fc5fea70f47a9651156c99c3973735859ed
ABiteOfSorrow <laceican@naver.com>
2023-03-09 18:56:38 +0100

    eventEmitter 2

--- emitter.js
+++ emitter.js
@@ -1,9 +1,18 @@
 const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
+const myEmitter1 = new EventEmitter();
 
 myEmitter.on('test', () => {
-    console.log('success');
+    console.log('success1');
+});
+
+myEmitter.on('test', () => {
+    console.log('success2');
+});
+
+myEmitter1.on('test', () => {
+    console.log('success3');
 });
 
 myEmitter.emit('test')
