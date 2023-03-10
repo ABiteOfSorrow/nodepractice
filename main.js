754cdf7f211863675cb42ec7bcf6a517eabe4717
ABiteOfSorrow <laceican@naver.com>
2023-03-10 16:05:51 +0100

    routing base

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
+users = ['Tom', 'Andy', 'Jessica', 'Paul'];
+
 let server = http.createServer(function (request, response) {
-    response.end('<h1>Hello World</h1>')
+
+    /* test 1. change url bar's text ex) http://127.0.0.1:3000/programming
+       response.end(`<h1>Hello ${request.url}</h1>}`) */
+
+    // test 2. 
+    // if there is nothing default url = "/""
+    if (request.url === "/") {
+        response.end(`<h1>Welcome developer</h1>`);
+    } else if (request.url === `/users`) {
+        response.end(`<h1>${users}</h1>`);
+    } else {
+        response.end(`<h1>Page Not Available</h1>`);
+    }
 });
 
 // 127.0.0.1:3000 or localhost:3000
